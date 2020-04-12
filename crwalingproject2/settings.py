827ac6256be59da596e38a4faceabea5dc2a4e31
taxijjang <gw9122@Naver.com>
2020-04-13 03:42:00 +0900

    헤로쿠 안되면 잔다

--- crwalingproject2/settings.py
+++ crwalingproject2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'a^qi6zk3ns5%56!1z%@7m+^$&gokkt
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
