73538ae910eac8abe38c182522e1799d1473f5eb
taxijjang <gw9122@Naver.com>
2020-04-13 16:44:24 +0900

    포트폴리오 만들어야댐

--- crwalingproject2/settings.py
+++ crwalingproject2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,14 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'a^qi6zk3ns5%56!1z%@7m+^$&gokkt=qzm&jlxh!q_)ls1zk7u'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'a^qi6zk3ns5%56!1z%@7m+^$&gokkt=qzm&jlxh!q_)ls1zk7u')
+SECRET_KEY = 'a^qi6zk3ns5%56!1z%@7m+^$&gokkt=qzm&jlxh!q_)ls1zk7u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,7 +124,3 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
